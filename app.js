b1c8ee33b9b2b53cc3678fdc924a48dce5ac772b
David Paul <davidpauljunior@gmail.com>
2017-01-06 16:32:06 +0000

    Adds data for friends list

--- app.js
+++ app.js
@@ -7,23 +7,62 @@ const app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => {
-    strava.athlete.get({},function(err, payload) {
+    // http://strava.github.io/api/v3/activities/#get-feed
+    // on page load can get first page, then can do ajax request for more at a later date
+
+    strava.activities.listFriends({}, (err, payload) => {
+        console.log('args here: \n', payload);
         if(!err) {
             res.render('home', {
-                username: payload.username,
+                data: payload,
                 props: util.inspect(payload, {showHidden: false, depth: null})
             });
-            console.log(payload);
-            // res.send(
-            //     payload
-            // );
         }
         else {
             console.log(err);
         }
-    })
+
+        // payload.forEach(activity => {
+        //     console.log('Athlete');
+        //     console.log(activity.athlete);
+        // });
+    });
+
+    // Note: atheles.get{id:123456} will get another user, but much less data (mostly useless oddly).
+    // strava.athletes.get({id:1736644}, function(err, payload) {
+    //     if(!err) {
+    //         res.render('home', {
+    //             username: payload.username,
+    //             props: util.inspect(payload, {showHidden: false, depth: null})
+    //         });
+    //         console.log(payload);
+    //         // res.send(
+    //         //     payload
+    //         // );
+    //     }
+    //     else {
+    //         console.log(err);
+    //     }
+    // });
+
+    // strava.activities.get({id:822362092},function(err, payload) {
+    //     if(!err) {
+    //         res.render('home', {
+    //             username: payload.username,
+    //             props: util.inspect(payload, {showHidden: false, depth: null})
+    //         });
+    //         console.log(payload);
+    //         // res.send(
+    //         //     payload
+    //         // );
+    //     }
+    //     else {
+    //         console.log(err);
+    //     }
+    // })
 });
 
 app.listen(3000, ()=> {
